5cf6d37682a6f494f2c541406e7e71426c45a0ed
Grant Haack <granthaack@hush.com>
2018-07-21 14:59:04 -0700

    added initialization of the accelerometer

--- defcon26/defcon26.ino
+++ defcon26/defcon26.ino
@@ -1,8 +1,12 @@
 #include <Adafruit_NeoPixel.h> 
 #include <RF24.h>
 #include "displays.h"
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 Adafruit_NeoPixel pixels;
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 uint8_t DRAW_OFFSET = 150;
 
@@ -17,12 +21,19 @@ void (*mainUpdate) (struct Pixel oldGrid[10][20], struct Pixel newGrid[10][20]) 
 void (*mainInit) (struct Pixel newGrid[10][20]) = displays[1].initFunc;
 
 void setup() {
+  Serial.begin(9600);
   randomSeed(1);
 
   //Set the pin as an input
   pinMode(A0, INPUT);
   //Turn on the internal pullup
   digitalWrite(A0, HIGH);
+
+  //Turn on the accelerometer
+  if (! lis.begin(0x19)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Accelerometer couldnt start");
+  }
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
   
   lastDrawTime = millis();
   pixels = Adafruit_NeoPixel(50, 6, NEO_GRB);
@@ -42,6 +53,9 @@ void setup() {
 }
 
 void loop() {
+  //Get accelerometer data
+  lis.read();
+
   // If we haven't calculated our new grid for this draw, do it.
   if (needsUpdate) {
     mainUpdate(oldGrid, newGrid);
